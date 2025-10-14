fb075cedf1aae80b1a380a4e6982761c335cb880
Asad Zaman <10asadzaman@gmail.com>
2025-10-14 14:58:41 +0600

    putted selected players in h1 tag

--- src/SelectedPlayers/SelectedPlayers.jsx
+++ src/SelectedPlayers/SelectedPlayers.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SelectedPlayers = () => {
     return (
         <div>
-            SelectedPlayers
+            <h1>Selected Players</h1>
         </div>
     );
 };
